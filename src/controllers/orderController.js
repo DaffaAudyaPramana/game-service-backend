5d45fece2c43defc3c4e239d6cea5c172528731e
daffaaudyapramana <daffaaudyapramana03@gmail.com>
2026-05-03 23:54:41 +0700

    update order & orderid

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -16,40 +16,55 @@ export const createOrder = async (req, res) => {
       notes,
     } = req.body;
 
-    // VALIDASI
     if (!productId || !totalPrice) {
       return res.status(400).json({
         error: "productId dan totalPrice wajib",
       });
     }
 
-    const order = await prisma.order.create({
-      data: {
-        orderId: generateOrderId(),
-        productId,
-        totalPrice,
-        status: "pending",
+    // 🔥 TRANSACTION
+    const result = await prisma.$transaction(async (tx) => {
 
-        //SIMPAN SEMUA
-        name,
-        method,
-        platform,
-        version,
-        gameUserId,
-        notes,
-      },
+      // 1. CREATE ORDER
+      const order = await tx.order.create({
+        data: {
+          orderId: generateOrderId(),
+          productId,
+          totalPrice,
+          status: "pending",
+
+          name,
+          method,
+          platform,
+          version,
+          gameUserId,
+          notes,
+        },
+      });
+
+      // 2. CREATE GTA ORDER
+      const gtaOrder = await tx.gTAOrder.create({
+        data: {
+          orderId: order.id, // 🔥 FK ke Order.id
+          serviceType: "gta-v",
+          targetAccount: gameUserId,
+          progress: 0,
+        },
+      });
+
+      return { order, gtaOrder };
     });
 
     res.status(200).json({
-      message: "Order berhasil dibuat",
-      data: order,
+      message: "Order + GTAOrder berhasil dibuat",
+      data: result,
     });
 
   } catch (error) {
     console.error(error);
 
     res.status(500).json({
-      error: "Gagal create order",
+      error: error.message,
     });
   }
 };
