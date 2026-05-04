3cdca38d83e038524416cf3df48323df51339442
daffaaudyapramana <daffaaudyapramana03@gmail.com>
2026-05-04 21:34:42 +0700

    update payment status & protect order

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,12 +3,17 @@ import express from "express";
 import { createOrder, getOrderByOrderId } from "../controllers/orderController.js";
 import upload from "../utils/upload.js";
 import { uploadPaymentProof } from "../controllers/orderController.js";
+import {
+  getAllOrders,
+  updatePaymentStatus,
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
 // endpoint
 router.get("/:orderId", getOrderByOrderId);
 router.post("/", createOrder);
+router.get("/", getAllOrders);
 
 // endpoint upload bukti
 router.post(
@@ -16,5 +21,10 @@ router.post(
   upload.single("file"),
   uploadPaymentProof
 );
+
+// approve / reject
+router.patch("/:orderId/payment", updatePaymentStatus);
+
+router.post("/", protect, createOrder);
 
 export default router;
