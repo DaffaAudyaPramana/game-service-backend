18d01806fe2ab8ef692d785d0182d2065edcd263
daffaaudyapramana <daffaaudyapramana03@gmail.com>
2026-05-04 21:33:46 +0700

    add auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,71 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+const JWT_SECRET = "SECRET_KEY"; // nanti pindah ke .env
+
+export const me = async (req, res) => {
+  res.json({
+    user: req.user,
+  });
+};
+
+// REGISTER
+export const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashed,
+      },
+    });
+
+    res.json({
+      message: "Register sukses",
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Register gagal" });
+  }
+};
+
+// LOGIN
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(404).json({ error: "User tidak ditemukan" });
+  }
+
+  const valid = await bcrypt.compare(password, user.password);
+
+  if (!valid) {
+    return res.status(401).json({ error: "Password salah" });
+  }
+
+  const token = jwt.sign(
+    { id: user.id, role: user.role },
+    JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  // 🔥 COOKIE LOGIN
+  res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "lax",
+  });
+
+  res.json({
+    message: "Login sukses",
+  });
+};
