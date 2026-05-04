ac639d69c1869b553c3b6a5d95960f968709e656
daffaaudyapramana <daffaaudyapramana03@gmail.com>
2026-05-04 21:36:25 +0700

    update nodemailer, getallorders, paymenstatus, send email

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import generateOrderId from "../utils/generateOrderId.js";
 import { fileTypeFromFile } from "file-type";
 import fs from "fs";
+import nodemailer from "nodemailer";
 
 const prisma = new PrismaClient();
 
@@ -158,3 +159,94 @@ export const uploadPaymentProof = async (req, res) => {
   }
 };
 
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await prisma.order.findMany({
+      include: {
+        payment: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    res.json({
+      message: "List orders",
+      data: orders,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Gagal ambil orders" });
+  }
+};
+
+export const updatePaymentStatus = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body; // "approved" | "rejected"
+
+    if (!["approved", "rejected"].includes(status)) {
+      return res.status(400).json({
+        error: "Status tidak valid",
+      });
+    }
+
+    const order = await prisma.order.findUnique({
+      where: { orderId },
+    });
+
+    if (!order) {
+      return res.status(404).json({
+        error: "Order tidak ditemukan",
+      });
+    }
+
+    // update payment
+    const payment = await prisma.payment.update({
+      where: { orderId: order.id },
+      data: {
+        status,
+      },
+    });
+
+    // update order juga
+    await prisma.order.update({
+      where: { id: order.id },
+      data: {
+        status: status === "approved" ? "completed" : "rejected",
+      },
+    });
+
+    res.json({
+      message: "Status berhasil diupdate",
+      data: payment,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "Gagal update status",
+    });
+  }
+};
+
+const sendEmail = async (to, orderId) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "email@gmail.com",
+      pass: "app_password",
+    },
+  });
+
+  await transporter.sendMail({
+    to,
+    subject: "Order Berhasil",
+    text: `Terima kasih telah order ${orderId}`,
+  });
+};
+
+const user = await prisma.user.findUnique({
+  where: { id: req.user.id },
+});
+
+await sendEmail(user.email, order.orderId);
