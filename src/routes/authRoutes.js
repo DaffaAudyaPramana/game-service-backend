332b53210346264456684b23b993daed84b43e3e
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2026-05-16 00:21:58 +0700

    update & add logout import, forgotPassword, resetPassword, router forgot, reset and logout

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { login, register, me } from "../controllers/authController.js";
+import {
+  login,
+  register,
+  me,
+  logout,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/authController.js";
 import { protect } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+
 router.get("/me", protect, me);
+router.post("/logout", logout);
 
 export default router;
