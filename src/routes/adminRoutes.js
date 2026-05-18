c3ce98554db86670aa9973a2de0b1b650bab4e8b
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2026-05-18 15:46:24 +0700

    update: add reward redeem admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,28 @@
 import express from "express";
-import { getAllUsersWithOrders } from "../controllers/adminController.js";
+import { 
+    getAllUsersWithOrders,
+    getRewardRedemptions,
+    updateRewardRedemptionStatus,
+ } from "../controllers/adminController.js";
 import { protect } from "../middlewares/auth.js";
 import { adminOnly } from "../middlewares/admin.js";
 
 const router = express.Router();
 
 router.get("/users", protect, adminOnly, getAllUsersWithOrders);
+
+router.get(
+  "/reward-redemptions",
+  protect,
+  adminOnly,
+  getRewardRedemptions
+);
+
+router.patch(
+  "/reward-redemptions/:redemptionId/status",
+  protect,
+  adminOnly,
+  updateRewardRedemptionStatus
+);
 
 export default router;
