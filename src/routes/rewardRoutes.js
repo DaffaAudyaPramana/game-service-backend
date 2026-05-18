c881466f9d6c8a776d1902d6f9dfcfb1e08df527
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2026-05-18 16:47:52 +0700

    update: add rewardLimiter into reward routes

--- src/routes/rewardRoutes.js
+++ src/routes/rewardRoutes.js
@@ -5,11 +5,12 @@ import {
   redeemReward,
 } from "../controllers/rewardController.js";
 import { protect } from "../middlewares/auth.js";
+import { rewardLimiter } from "../middlewares/rateLimit.js";
 
 const router = express.Router();
 
 router.get("/", getRewards);
 router.get("/me", protect, getMyPoints);
-router.post("/:rewardId/redeem", protect, redeemReward);
+router.post("/:rewardId/redeem", protect, rewardLimiter, redeemReward);
 
 export default router;
