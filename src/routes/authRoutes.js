6b6cb6b69b98fa488571b9a25057c14463be304f
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2026-05-18 16:49:17 +0700

    update: add security authLimiter into auth route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,16 +8,20 @@ import {
   resetPassword,
 } from "../controllers/authController.js";
 import { protect } from "../middlewares/auth.js";
+import {
+  authLimiter,
+  forgotPasswordLimiter,
+} from "../middlewares/rateLimit.js";
 
 const router = express.Router();
 
-router.post("/login", login);
-router.post("/register", register);
+router.post("/login", authLimiter, login);
+router.post("/register", authLimiter, register);
 
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password", resetPassword);
+router.post("/forgot-password", forgotPasswordLimiter, forgotPassword);
+router.post("/reset-password", forgotPasswordLimiter, resetPassword);
 
 router.get("/me", protect, me);
-router.post("/logout", logout);
+router.post("/logout", protect, logout);
 
 export default router;
